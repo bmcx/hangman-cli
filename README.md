00689332f3190d1ff32665a6ec1f6f6425738947
unknown <bmcbb2334@gmail.com>
2020-02-23 13:54:41 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# hangman-cli
-Popular hangman game recreated with python 
+# Hangman CLI
+
+
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/) [![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+
+Hangman is a popular word game,and using python I created a CLI version of it, 
+
+### Preview :computer:
+
+![GIF](preview.gif)
+
+
+
+### Setup :wrench:
+
+- Clone this repo to your local machine using 
+`git clone https://github.com/dev-pos/hangman-cli.git`
+
+- Then simply run the script using 
+`python hangman.py`
+
+
+### TODO :clipboard: 
+- [ ] Create a Game Menu 
+- [ ] Dificulty Levels, Hints, Scoreboard etc...
+- [ ] Maybe sounds? :wink:
+
+---
+
+### License :scroll:
+
+- **[MIT license](http://opensource.org/licenses/mit-license.php)**
---
+++ hangman.py
@@ -0,0 +1,119 @@
+import random
+man = [
+"""______
+|    |
+|    |
+|    
+|   
+|     
+|   
+|
+|=========""",
+"""______
+|    |
+|    |
+|    \O
+|   
+|     
+|   
+|
+|=========""",
+"""______
+|    |
+|    |
+|    \O
+|     |
+|     |  
+|    
+|
+|=========""",
+"""______
+|    |
+|    |
+|    \O
+|   \/|
+|     |  
+|    
+|
+|=========""",
+"""______
+|    |
+|    |
+|    \O
+|   \/|\/
+|     |  
+|    
+|
+|=========""",
+"""______
+|    |
+|    |
+|    \O
+|   \/|\/
+|     |  
+|   _/ 
+|
+|=========""",
+"""______
+|    |
+|    |
+|    \O
+|   \/|\/
+|     |  
+|   _/ \_
+|
+|========="""
+]
+
+def drawLevel(man, word, mistakes, answer, letterCount, tried, score):
+    print(man[mistakes])
+    print(' '.join(answer) if letterCount > 0 else ' '.join(answer)+"\n\nCongratulations!!\n The word is correct" )
+    print( "\nSorry you are DEAD!! x_x \n The word is "+word if mistakes >= 6 else '')
+    if(tried and letterCount > 0):
+        print("✘ : " + ' '.join(tried))
+
+def getInput(tried):
+    ok = False
+    while not ok:
+        letter = str(input('\nTry a letter: ')).lower()
+        if not(letter in tried):
+            ok = True
+            return letter
+        print("You already tried '"+letter+"'")
+    
+
+
+play = True
+score = 0
+while(play):
+
+    with open('words.txt') as f:
+        words = f.readlines()
+        word = words[random.randrange(0, 80000)]
+
+    letterCount = len(word)-1
+    mistakes = 0
+    tried = []
+    rand = random.randrange(1,len(word))
+
+    out = [ ( word[i-1] if ( rand == i ) else '_' ) for i in range(1,len(word)) ]
+
+    while(letterCount > 0 and mistakes < 6):
+        letter  = word[rand-1] if(letterCount == len(word)-1) else getInput(tried)
+
+        tried.append(letter)
+        for idx,el in enumerate(word):
+            if el == letter:
+                out[idx] = el
+                letterCount -= 1
+        
+        if letter not in word:
+            mistakes += 1
+
+        drawLevel(man, word, mistakes, out, letterCount, tried, score)
+
+    score = score+len(word)*2 if mistakes < 6 else score
+    
+    ask = input("Score : "+str(score)+"\nAny key to continue, Q to quit : ")
+    if ask.lower() == 'q':
+        exit()
